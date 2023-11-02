75fa4318f7b14ce6b3669cec0ed727bebdf933c3
Hrishikesh Bhorde <bhordehrishikesh@gmail.com>
2023-11-02 15:07:43 +0530

    Commit 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
+  const [result, setResult] = useState(0);
+
+  const handleInput1Change = (event) => {
+    setInput1(event.target.value);
+  };
+
+  const handleInput2Change = (event) => {
+    setInput2(event.target.value);
+  };
+
+  const addNumbers = () => {
+    if ((input1 === "") | (input2 === "")) {
+      setResult("Input values required");
+    } else setResult(parseFloat(input1) + parseFloat(input2));
+  };
+
+  const subtractNumbers = () => {
+    if ((input1 === "") | (input2 === "")) {
+      setResult("Input values required");
+    } else setResult(parseFloat(input1) - parseFloat(input2));
+  };
+  const multiplyNumbers = () => {
+    if ((input1 === "") | (input2 === "")) {
+      setResult("Input values required");
+    } else setResult(parseFloat(input1) * parseFloat(input2));
+  };
+
+  const divideNumbers = () => {
+    if (input2 === "0") {
+      setResult("Cannot divide by zero");
+    } else {
+      setResult(parseFloat(input1) / parseFloat(input2));
+    }
+  };
+
+  const percentNumbers = () => {
+    if ((input1 === "") | (input2 === "")) {
+      setResult("Input values required");
+    } else setResult(parseFloat(input1) % parseFloat(input2));
+  };
+
+  const reset = () => {
+    setInput1("");
+    setInput2("");
+    setResult(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{ color: "#b30000" }}>Calculator App</h1>
+      <input
+        className="cus-inp"
+        type="number"
+        placeholder="Num 1"
+        name="num1"
+        value={input1}
+        onChange={handleInput1Change}
+      ></input>
+      <br />
+      <input
+        className="cus-inp"
+        type="number"
+        placeholder="Num 2"
+        name="num2"
+        value={input2}
+        onChange={handleInput2Change}
+      ></input>
+      <br />
+      {/* <p className="ans_div">Ans</p> */}
+      <hr style={{ border: "1px solid" }} />
+      <b className="res">Ans : {result}</b>
+      <hr style={{ border: "1px solid" }} />
+      <br />
+      <button className="cus-btn" onClick={addNumbers}>
+        +
+      </button>
+      <button className="cus-btn" onClick={subtractNumbers}>
+        -
+      </button>
+      <button className="cus-btn" onClick={divideNumbers}>
+        /
+      </button>
+      <button className="cus-btn" onClick={multiplyNumbers}>
+        X
+      </button>
+      <button className="cus-btn" onClick={percentNumbers}>
+        %
+      </button>
+      <br></br>
+      <button onClick={reset} className="reset-btn">
+        RESET
+      </button>
     </div>
   );
 }
